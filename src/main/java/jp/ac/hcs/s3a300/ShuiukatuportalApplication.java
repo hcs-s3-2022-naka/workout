72c261fbd9fa0691e0634d2ff70179aaf653cf41
nakata <nakata@nakata-pc>
2022-03-24 10:43:07 +0900

    最初のコミットです

---
+++ src/main/java/jp/ac/hcs/s3a300/ShuiukatuportalApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a300;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuiukatuportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuiukatuportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/s3a300/ShuiukatuportalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a300;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuiukatuportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
